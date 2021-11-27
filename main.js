3b2b27b8f269da7640ce051810fc0f2f3c896175
Ani-wolf <83391971+Ani-wolf@users.noreply.github.com>
2021-11-27 21:49:36 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,28 @@ function preload() {
 function setup() {
     canvas = createCanvas(300, 300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    
+    poseNet = ml5poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results)
+{
+    if(results.length . 0)
+    {
+        console.log(results);
+        console.log("lips x = " + results[0].pose.lips.x);
+        console.log("lips y = " + results[0].pose.lips.y);
+
+function modelLoaded() {
+console.log('PoseNet Is Initialized');
 }
 
 function draw() {
+    image(video, 0, 0, 300, 300);
 }
 
 function take_snapshot() {
