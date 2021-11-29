8726339430b69cdf3d1192932c865955c57bf633
Ani-wolf <83391971+Ani-wolf@users.noreply.github.com>
2021-11-29 14:40:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,43 @@
+lipsX=0;
+lipsY=0;
+
 function preload() {
+    lipstick = loadImage('https://i.postimg.cc/mrmh3YBF/lipstick.jpg');
 }
 
 function setup() {
     canvas = createCanvas(300, 300);
     canvas.center();
     video = createCapture(VIDEO);
-    video.size(300, 300);
+    video.size(300,300);
     video.hide();
-    
+
     poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
 }
 
 function gotPoses(results)
 {
     if(results.length > 0)
     {
-        console.log(results);
-        console.log("lips x = " + results[0].pose.lips.x);
-        console.log("lips y = " + results[0].pose.lips.y);
+    console.log(results);
+    lipsX = results[0].pose.lips.x;
+    lipsY = results[0].pose.lips.y;
+    console.log("lips x = " + lipsX);
+    console.log("lips y = " + lipsY);
     }
-}
-
-function modelLoaded() {
-console.log('PoseNet Is Initialized');
 }
 
 function draw() {
     image(video, 0, 0, 300, 300);
+    //fill(255,0,0);
+    //stroke(255,0,0);
+    //circle(lipsX, lipsY, 20);
+    image(lipstick, lipsY, lipsY, 30, 30);
 }
 
 function take_snapshot() {
